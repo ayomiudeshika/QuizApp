9b9abf82620e8816b6a2d2b37ed553547360498b
ayomiudeshika <ayomiudeshika19@gmail.com>
2024-01-12 01:03:28 +0530

    Initial commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,27 @@
+export const Questions = [
+    {
+      prompt: "What is the process of finding errors and fixing them within a program?",
+      optionA: "Scanning",
+      optionB: "Executing",
+      optionC: "Compiling",
+      optionD: "Debugging",
+      asnwer: "optionD",
+    },
+    {
+      prompt: "The pseudocode is a ...",
+      optionA: "flowchart",
+      optionB: "object code",
+      optionC: "algorithm",
+      optionD: "coding",
+      asnwer: "optionC",
+    },
+    {
+      prompt: "Which of this is not a javascript framework?",
+      optionA: "React",
+      optionB: "Angular",
+      optionC: "Vue",
+      optionD: "Java",
+      asnwer: "optionD",
+    },
+  ];
+  
